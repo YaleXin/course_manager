d2048aeaf082b8964a4f8e120eda6c9c70e5d012
Yalexin_mac <495592364@qq.com>
2020-12-22 17:14:31 +0800

    题目添加 题目请求初步完成

--- src/components/InputSubject.vue
+++ src/components/InputSubject.vue
@@ -7,14 +7,16 @@
       label-width="100px"
       class="demo-ruleForm"
     >
-      <el-form-item label="题目类别" prop="category">
-        <el-input v-model="ruleForm.category"></el-input>
+      <el-form-item label="题目名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
       <el-form-item label="题目描述" prop="desc">
-        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+        <el-input type="textarea" v-model="ruleForm.desc" :autosize="{ minRows: 10, maxRows: 20}"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >立即创建</el-button
+        >
         <el-button type="primary" plain @click="back">返回</el-button>
       </el-form-item>
     </el-form>
@@ -27,12 +29,12 @@ export default {
   data() {
     return {
       ruleForm: {
-        category: "",
+        name: "",
         desc: "",
       },
       rules: {
-        category: [
-          { required: true, message: "请输入分类名称", trigger: "blur" },
+        name: [
+          { required: true, message: "请输入题目名称", trigger: "blur" },
           {
             min: 1,
             max: 10,
@@ -40,16 +42,49 @@ export default {
             trigger: "blur",
           },
         ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
+        desc: [{ required: true, message: "请填写描述", trigger: "blur" }],
       },
     };
   },
   mounted() {},
   methods: {
+    addSuccess() {
+      this.$message({
+        showClose: true,
+        message: "添加成功",
+        type: "success",
+      });
+    },
+    addFaile(msg = "") {
+      this.$message({
+        showClose: true,
+        message: "添加失败，失败原因：" + msg,
+        type: "error",
+      });
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert("submit!");
+          this.$axios
+            .put("/addSubject.te", {
+              data: {
+                name: this.ruleForm.name,
+                description: this.ruleForm.desc,
+                t_id: this.$store.state.user.id,
+              },
+            })
+            .then((resp) => {
+              if (resp.data.addSuccess) {
+                this.addSuccess();
+              } else {
+                this.addFaile(resp.data.error);
+              }
+            })
+            .catch((e) => {
+              this.addFaile();
+            });
+          //console.log("user:");
+          //console.log(this.$store.state.user); // {id: , gender: , username}
         } else {
           console.log("error submit!!");
           return false;
--- src/components/Subject.vue
+++ src/components/Subject.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="">
-    <el-table :data="programs" stripe style="width: 100%">
-      <el-table-column prop="descr" label="题目简述" width="180"> </el-table-column>
-      <el-table-column prop="selectNum" label="选题人数" width="180"> </el-table-column>
+    <el-table :data="subjects" stripe style="width: 100%">
+      <el-table-column prop="name" label="题目名称" width="180">
+        <template slot-scope="scope">
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="点击查看题目详情"
+            placement="top"
+          >
+            <el-button @click.native="SubjectClick(scope.$index)">{{
+              scope.row.name
+            }}</el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+      <el-table-column prop="selectNum" label="选题人数" width="180">
+      </el-table-column>
       <el-table-column prop="finished" label="完成人数"> </el-table-column>
-      <el-table-column prop="copyright" label="题目提供人"> </el-table-column>
+      <el-table-column prop="teacherName" label="题目提供者"> </el-table-column>
     </el-table>
     <el-button type="success" @click="tryAddSubject" plain id="add-btn"
       >添加题目</el-button
     >
+    <el-dialog
+      title="题目详情"
+      :visible.sync="centerDialogVisible"
+      width="80%"
+      center
+    >
+      <pre>{{description}}</pre>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -21,6 +49,17 @@ export default {
         { descr: "题目一", selectNum: 10, finished: 5, copyright: "黄阿信" },
         { descr: "题目二", selectNum: 10, finished: 2, copyright: "黄阿信" },
       ],
+      subjects: [
+        {
+          name: "题目名称",
+          selectNum: 0,
+          finished: 0,
+          teacherName: "题目提供者",
+          description: "题目描述",
+        },
+      ],
+      description: "",
+      centerDialogVisible: false
     };
   },
   mounted() {},
@@ -28,6 +67,20 @@ export default {
     tryAddSubject() {
       this.$router.push("/addSubject").catch((e) => {});
     },
+    SubjectClick(index) {
+      this.description = this.subjects[index].description;
+      this.centerDialogVisible = true;
+    },
+  },
+  created() {
+    this.$axios
+      .get("/getAllSubject")
+      .then((resp) => {
+        this.subjects = resp.data.subjects;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   },
 };
 </script>
